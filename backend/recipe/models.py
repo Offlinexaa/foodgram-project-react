4357af33ceb46a4647b49334add0e33c6498f86f
Offlinexaa <offlinexaa@yandex.ru>
2022-06-03 01:52:08 +0400

    Модель recipe в работе

--- backend/recipe/models.py
+++ backend/recipe/models.py
@@ -1,12 +1,28 @@
 """Модуль содержит модели и их настройки для приложения recipe."""
 from django.contrib.auth import get_user_model
-from django.db import models
+from django.db.models import (Model, ForeignKey, CASCADE, ManyToManyField,
+                              PositiveIntegerField,)
+from django.core.validators import MinValueValidator
 
 User = get_user_model()
 
 
-class Recipe:
-    pass
+class Recipe(Model):
+    author = ForeignKey(
+        to=User,
+        on_delete=CASCADE,
+        verbose_name='Автор рецепта',
+        related_name='recipes',        
+    )
+    by_list = ManyToManyField(
+        to=User,
+        verbose_name='Список покупок',
+        related_name='by_lists',
+    )
+    cooking_time = PositiveIntegerField(
+        verbose_name='Время приготовдения',
+        default=0,
+    )
 
 
 class Ingredient:
