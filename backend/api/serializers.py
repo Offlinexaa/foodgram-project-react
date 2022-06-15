48187a62edc37e6a0c82bbc0bf1b0a7cbda1a506
Offlinexaa <Offlinexaa@gmail.com>
2022-06-15 22:53:55 +0400

    В работе сериализаторы

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,13 +1,15 @@
 from re import match
+from typing import Any
 
+from drf_extra_fields.fields import Base64ImageField
 from django.contrib.auth import get_user_model
+from django.db.models import F
 from rest_framework.serializers import (ModelSerializer, SerializerMethodField,
                                         ValidationError)
 
 from recipe.models import Ingredient, Recipe, Tag
 from api.validators import class_obj_validate, hex_color_validate
 from api.utils import recipe_amount_ingredients_set
-
 
 User = get_user_model()
 
@@ -139,3 +141,147 @@ class TagSerializer(ModelSerializer):
         color = str(color).strip(' #')
         hex_color_validate(color)
         return f'#{color}'
+
+
+class RecipeSerializer(ModelSerializer):
+    """
+    Сериализатор для модели Recipe.
+    """
+    tags = TagSerializer(many=True, read_only=True)
+    author = UserSerializer(read_only=True)
+    ingredients = SerializerMethodField()
+    is_favorited = SerializerMethodField()
+    is_in_shopping_cart = SerializerMethodField()
+    image = Base64ImageField()
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id',
+            'tags',
+            'author',
+            'ingredients',
+            'name',
+            'image',
+            'text',
+            'cooking_time',
+            'is_favorited',
+            'is_in_shopping_cart',
+        )
+        read_only_fields = (
+            'is_favorite',
+            'is_shopping_cart',
+        )
+
+    def get_ingredients(self, obj: object) -> Any:
+        """
+        Возвращает список ингридиентов для рецепта.
+        """
+        ingredients = obj.ingredients.values(
+            'id',
+            'name',
+            'measurement_unit',
+            amount=F('recipe__amount'),
+        )
+        return ingredients
+
+    def get_is_favorited(self, obj: object) -> bool:
+        """
+        Возвращает True если переданный объект находится в избранном у
+        текущего пользователя.
+        """
+        user = self.context.get('request').user
+        if user.is_authenticated:
+            return user.favorites.filter(id=obj.id).exists()
+        return False
+
+    def get_is_in_shopping_cart(self, obj: object) -> bool:
+        """
+        Возвращает True если переданный объект находится в списке покупок у
+        текущего пользователя.
+        """
+        user = self.context.get('request').user
+        if user.is_authenticated:
+            return user.in_cart.filter(id=obj.id).exists()
+        return False
+
+    def validate(self, data):
+        """
+        Проверка вводных данных при создании/редактировании рецепта.
+        """
+        name = str(self.initial_data.get('name')).strip()
+        tags = self.initial_data.get('tags')
+        ingredients = self.initial_data.get('ingredients')
+        values_as_list = (tags, ingredients)
+
+        for value in values_as_list:
+            if not isinstance(value, list):
+                raise ValidationError(
+                    f'Содержимое "{value}" должно быть списком.'
+                )
+
+        for tag in tags:
+            class_obj_validate(
+                value=tag,
+                klass=Tag
+            )
+
+        valid_ingredients = []
+        for ingredient in ingredients:
+            ingredient_id = ingredient.get('id')
+            ingredient = class_obj_validate(
+                value=ingredient_id,
+                klass=Ingredient
+            )
+            amount = ingredient.get('amount')
+            class_obj_validate(amount)
+            valid_ingredients.append(
+                {
+                    'ingredient': ingredient,
+                    'amount': amount,
+                }
+            )
+
+        data['name'] = name.lower()
+        data['tags'] = tags
+        data['ingredients'] = valid_ingredients
+        data['author'] = self.context.get('request').user
+        return data
+
+    def create(self, validated_data):
+        """
+        Создаёт новый объект модели Recipe.
+        """
+        image = validated_data.pop('image')
+        tags = validated_data.pop('tags')
+        ingredients = validated_data.pop('ingredients')
+        recipe = Recipe.objects.create(image=image, **validated_data)
+        recipe.tags.set(tags)
+        recipe_amount_ingredients_set(recipe, ingredients)
+        return recipe
+
+    def update(self, recipe, validated_data):
+        """
+        Обновляет объект Recipe.
+        """
+        tags = validated_data.get('tags')
+        ingredients = validated_data.get('ingredients')
+
+        recipe.image = validated_data.get('image', recipe.image)
+        recipe.name = validated_data.get('name', recipe.name)
+        recipe.text = validated_data.get('text', recipe.text)
+        recipe.cooking_time = validated_data.get(
+            'cooking_time',
+            recipe.cooking_time
+        )
+
+        if tags:
+            recipe.tags.clear()
+            recipe.tags.set(tags)
+
+        if ingredients:
+            recipe.ingredients.clear()
+            recipe_amount_ingredients_set(recipe, ingredients)
+
+        recipe.save()
+        return recipe
