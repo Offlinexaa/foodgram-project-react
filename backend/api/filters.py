9d57b83b121fc87c01f40224be788af0a89f5e22
Offlinexaa <Offlinexaa@gmail.com>
2022-07-28 21:34:00 +0400

    Фильтры вынесены в отдельный модуль

---
+++ backend/api/filters.py
@@ -0,0 +1,50 @@
+from django_filters.rest_framework import (FilterSet, CharFilter,
+                                           BooleanFilter, NumberFilter,
+                                           AllValuesMultipleFilter)
+
+from recipe.models import Ingredient, Recipe
+
+
+class IngredientFilter(FilterSet):
+    name = CharFilter(lookup_expr='istartswith')
+
+    class Meta:
+        model = Ingredient
+        fields = ('name', )
+
+
+class RecipeFilter(FilterSet):
+    is_favorited = BooleanFilter(
+        method='get_is_favorited',
+    )
+    is_in_shopping_cart = BooleanFilter(
+        method='get_is_in_shopping_cart',
+    )
+    author = NumberFilter(
+        field_name='author__id',
+        lookup_expr='exact'
+    )
+    tags = AllValuesMultipleFilter(
+        field_name='tags__slug',
+    )
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'is_favorited',
+            'is_in_shopping_cart',
+            'author',
+            'tags'
+        )
+
+    def get_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if value:
+            return Recipe.objects.filter(favorite=user)
+        return Recipe.objects.all()
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value:
+            return Recipe.objects.filter(shopping_cart=user)
+        return Recipe.objects.all()
