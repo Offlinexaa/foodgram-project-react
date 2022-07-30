dcb9126fc306247f498baf3b1cb87677da6a38d3
Offlinexaa <Offlinexaa@gmail.com>
2022-07-30 12:42:18 +0400

    Подготовка к переходу на actions

--- infra/update_backend.sh
+++ infra/update_backend.sh
@@ -5,6 +5,3 @@ sleep 5
 docker-compose rm -f backend
 docker image rm infra_backend
 docker-compose up -d
-# sleep 5
-# docker-compose exec backend python manage.py createsuperuser --username admin --email a@a.local --noinput
-# docker-compose exec backend python manage.py load_data
